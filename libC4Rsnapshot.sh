b338a430a5c53176a840bec031736a272f1d710f
jeffbrite <jeff@c4tech.com>
2013-02-22 12:57:38 -0600

    Update libC4Rsnapshot.sh

--- libC4Rsnapshot.sh
+++ libC4Rsnapshot.sh
@@ -1,3 +1,4 @@
+# Version 1.0
 LOG="/var/log/rsnapshot.log"
 
 date2stamp () {
@@ -29,4 +30,25 @@ function logThis {
         output="$3"
         DATE=`date +%d/%b/%Y:%H:%M:%S`
         echo "[$DATE] $HOST - $mod: $output" >> $LOG
+}
+
+function runThis {
+        HOST=$1
+        MOD="$2"
+        FILENAME=$3
+        COMMAND=$4
+
+        logThis $HOST $MOD "Starting Backup"
+        if [ "localhost" = "$HOST" ]; then
+                ERROR=$( { $COMMAND > $FILENAME; RETURN=$?; } 2>&1 )
+        else
+                ERROR=$( { ssh $HOST $COMMAND > $FILENAME; RETURN=$?; } 2>&1 )
+        fi
+
+        if [ $RETURN ]; then
+                logThis $HOST $MOD "ERROR command returned $RETURN"
+        fi
+
+        logThis $HOST $MOD "Finnished"
+        chmod 600 $FILENAME
 }
